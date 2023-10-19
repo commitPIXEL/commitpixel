64ec28ef8f4d27731b2325940745fe2fc62d85fb
이용현 <3246lyh@naver.com>
2023-10-19 16:00:31 +0900

    fix: Response 방식 변경

--- backend/rankms/src/main/java/com/ssafy/realrealfinal/rankms/common/exception/ErrorCode.java
+++ backend/rankms/src/main/java/com/ssafy/realrealfinal/rankms/common/exception/ErrorCode.java
@@ -1,20 +1,15 @@
 package com.ssafy.realrealfinal.rankms.common.exception;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 
+@Getter
+@RequiredArgsConstructor
 public enum ErrorCode {
 
-    UNEXPECTED_ERROR(HttpStatus.BAD_REQUEST, "0"); // 예시
+    UNEXPECTED_ERROR("커스텀 에러 메시지", HttpStatus.BAD_REQUEST); // 예시
 
-    private final HttpStatus httpStatus;
     private final String message;
-
-    ErrorCode(HttpStatus httpStatus, String message) {
-        this.httpStatus = httpStatus;
-        this.message = message;
-    }
-
-    public String getMessage() {
-        return message;
-    }
+    private final HttpStatus httpStatus;
 }
