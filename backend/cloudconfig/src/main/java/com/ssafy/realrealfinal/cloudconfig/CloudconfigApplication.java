a944d1d2e34e2ccbe9db14a80d2215897979809d
이용현 <3246lyh@naver.com>
2023-10-20 17:23:38 +0900

    init: cloudconfig 프로젝트 생성

---
+++ backend/cloudconfig/src/main/java/com/ssafy/realrealfinal/cloudconfig/CloudconfigApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.realrealfinal.cloudconfig;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudconfigApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudconfigApplication.class, args);
+	}
+
+}
