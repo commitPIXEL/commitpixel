a7efd9934848de91c2eca9df777692f098911668
Amyhds <dasomhan524@gmail.com>
2023-10-22 22:04:16 +0900

    feat: 깃허브 커밋 수 얻어서 필터링

--- backend/userms/src/main/java/com/ssafy/realrealfinal/userms/common/util/GithubUtil.java
+++ backend/userms/src/main/java/com/ssafy/realrealfinal/userms/common/util/GithubUtil.java
@@ -1,9 +1,14 @@
-package com.ssafy.realrealfinal.userms.api.user.util;
+package com.ssafy.realrealfinal.userms.common.util;
 
 import com.fasterxml.jackson.databind.JsonNode;
+import java.time.Duration;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
 import lombok.extern.slf4j.Slf4j;
+import org.json.JSONObject;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @Slf4j
@@ -18,10 +23,10 @@ public class GithubUtil {
 
     /**
      * @param accessToken
-     * @param githubNickname
+     * @param userName
      * @return
      */
-    public Integer getCommit(String accessToken, String githubNickname) {
+    public Mono<Integer> getCommit(String accessToken, String userName) {
 
         // webclient로 github api 호출
         Mono<JsonNode> githubEventList = githubWebClient
@@ -29,14 +34,26 @@ public class GithubUtil {
             .uri(uriBuilder -> uriBuilder
                 .path("/users/{username}/events")
                 .queryParam("per_page", 100)
-                .build(githubNickname))
+                .build(userName))
             .header("Authorization", "Bearer " + accessToken)
             .retrieve()
             .bodyToMono(JsonNode.class);
 
-        // TODO: type, created_at 필터링
-
-        return null;
+        // type = pushEvent, created_at < 7 days 필터링
+        return githubEventList
+            .flatMapMany(jsonNode -> Flux.fromIterable(jsonNode))
+            .filter(event -> "PushEvent".equals(event.path("type").asText()))
+            .filter(event -> {
+                ZonedDateTime eventTime = ZonedDateTime.parse(
+                    event.path("created_at").asText(),
+                    DateTimeFormatter.ISO_OFFSET_DATE_TIME);
+                ZonedDateTime now = ZonedDateTime.now();
+                return Duration.between(eventTime, now).toDays() <= 7;
+            })
+            .count()
+            // 최근 90일 이내 아무런 이벤트가 없다면 0 리턴
+            .defaultIfEmpty(0L)
+            .map(Long::intValue);
     }
 
 }
