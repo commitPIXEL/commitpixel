11c7d304e1158e4610cffaf7215b5e3b058086c7
Amyhds <dasomhan524@gmail.com>
2023-10-22 23:39:46 +0900

    feat: redis 함수 구현 크레딧 데이터 특성 상 hashOperation으로 구현함

--- backend/userms/src/main/java/com/ssafy/realrealfinal/userms/common/util/RedisUtil.java
+++ backend/userms/src/main/java/com/ssafy/realrealfinal/userms/common/util/RedisUtil.java
@@ -1,5 +1,27 @@
-package com.ssafy.realrealfinal.userms.api.user.util;
+package com.ssafy.realrealfinal.userms.common.util;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.core.HashOperations;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@RequiredArgsConstructor
+@Component
 public class RedisUtil {
+
+    private final RedisTemplate RedisTemplate;
+
+    public Integer getData(String key, String type) {
+        HashOperations<String, String, Integer> hashOperations = RedisTemplate.opsForHash();
+        Integer data = hashOperations.get(key, type);
+        return data;
+    }
+
+    public void setData(String key, String type, Integer value) {
+        HashOperations<String, String, Integer> hashOperations = RedisTemplate.opsForHash();
+        hashOperations.put(key, type, value);
+    }
 
 }
