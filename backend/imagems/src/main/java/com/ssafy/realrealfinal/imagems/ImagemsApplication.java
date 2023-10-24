481333630135dc78f0eb12bff803ad85d65e5806
heejo <heejo515@naver.com>
2023-10-24 10:02:14 +0900

    feat: auth feign 연결

--- backend/imagems/src/main/java/com/ssafy/realrealfinal/imagems/ImagemsApplication.java
+++ backend/imagems/src/main/java/com/ssafy/realrealfinal/imagems/ImagemsApplication.java
@@ -2,8 +2,10 @@ package com.ssafy.realrealfinal.imagems;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@EnableFeignClients
 @EnableJpaRepositories(basePackages = "com.ssafy.realrealfinal")
 @SpringBootApplication
 public class ImagemsApplication {
--- backend/imagems/src/main/java/com/ssafy/realrealfinal/imagems/api/image/service/ImageServiceImpl.java
+++ backend/imagems/src/main/java/com/ssafy/realrealfinal/imagems/api/image/service/ImageServiceImpl.java
@@ -1,5 +1,6 @@
 package com.ssafy.realrealfinal.imagems.api.image.service;
 
+import com.ssafy.realrealfinal.imagems.api.image.feignInterface.ImageClient;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -8,5 +9,6 @@ import org.springframework.stereotype.Service;
 @RequiredArgsConstructor
 @Service
 public class ImageServiceImpl implements ImageService {
+    ImageClient imageClient;
 
 }
