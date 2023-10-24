793fccfa739d19046ea6cb3781121bba704c09fd
Dohyun822 <yup8229@gmail.com>
2023-10-25 06:43:21 +0900

    feat: socket.io 라이브러리 사용한 웹소켓 구현

---
+++ backend/pixelms/src/main/java/com/ssafy/realrealfinal/pixelms/api/pixel/dto/PixelDTO.java
@@ -0,0 +1,10 @@
+package com.ssafy.realrealfinal.pixelms.api.pixel.dto;
+
+
+public class PixelDTO {
+    private int x; //x 좌표
+    private int y; //y 좌표
+    private int r; //red 0~255
+    private int g; //green 0~255
+    private int b; //blue 0~255
+}
---
+++ backend/pixelms/src/main/java/com/ssafy/realrealfinal/pixelms/config/WebSocketConfig.java
@@ -0,0 +1,23 @@
+package com.ssafy.realrealfinal.pixelms.config;
+
+
+import com.corundumstudio.socketio.SocketIOServer;
+import com.corundumstudio.socketio.annotation.SpringAnnotationScanner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class WebSocketConfig {
+    @Bean
+    public SocketIOServer socketIoServer() {
+        //Configuration 충돌 해결을 위해 풀패키지 이름 사용
+        com.corundumstudio.socketio.Configuration config = new com.corundumstudio.socketio.Configuration();
+        config.setHostname("localhost");
+        config.setPort(3001);
+        return new SocketIOServer(config);
+    }
+    @Bean
+    public SpringAnnotationScanner springAnnotationScanner(SocketIOServer socketIOServer){
+        return new SpringAnnotationScanner(socketIOServer);
+    }
+}
