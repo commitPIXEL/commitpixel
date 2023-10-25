a09d536038cf15b6bb04365e28aa1399a8640e55
yup8229@gmail.com <yup8229@gmail.com>
2023-10-25 09:44:11 +0900

    fix: pixelDto에 url, userId 추가

--- backend/pixelms/src/main/java/com/ssafy/realrealfinal/pixelms/api/pixel/dto/PixelDto.java
+++ backend/pixelms/src/main/java/com/ssafy/realrealfinal/pixelms/api/pixel/dto/PixelDto.java
@@ -1,11 +1,13 @@
 package com.ssafy.realrealfinal.pixelms.api.pixel.dto;
 
 
-public class PixelDTO {
+public class PixelDto {
 
     private int x; //x 좌표
     private int y; //y 좌표
     private int r; //red 0~255
     private int g; //green 0~255
     private int b; //blue 0~255
+    private String url; //url 주소
+    private String userId; // 유저아이디
 }
