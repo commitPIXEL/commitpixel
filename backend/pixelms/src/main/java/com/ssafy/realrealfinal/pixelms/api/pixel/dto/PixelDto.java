463346e3aa5afec79f9948e8390ac8986f604ef7
yup8229@gmail.com <yup8229@gmail.com>
2023-10-25 09:51:03 +0900

    fix: int에서 Integer로 변경

--- backend/pixelms/src/main/java/com/ssafy/realrealfinal/pixelms/api/pixel/dto/PixelDto.java
+++ backend/pixelms/src/main/java/com/ssafy/realrealfinal/pixelms/api/pixel/dto/PixelDto.java
@@ -3,11 +3,11 @@ package com.ssafy.realrealfinal.pixelms.api.pixel.dto;
 
 public class PixelDto {
 
-    private int x; //x 좌표
-    private int y; //y 좌표
-    private int r; //red 0~255
-    private int g; //green 0~255
-    private int b; //blue 0~255
+    private Integer x; //x 좌표
+    private Integer y; //y 좌표
+    private Integer r; //red 0~255
+    private Integer g; //green 0~255
+    private Integer b; //blue 0~255
     private String url; //url 주소
     private String userId; // 유저아이디
 }
