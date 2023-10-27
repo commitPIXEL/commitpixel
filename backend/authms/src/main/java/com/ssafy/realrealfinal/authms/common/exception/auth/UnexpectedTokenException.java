9c6ff3fddbf5422adc460fa6d37ccf91993bf46e
yup8229@gmail.com <yup8229@gmail.com>
2023-10-27 17:11:15 +0900

    setting: pixel브랜치 정화작업

--- backend/authms/src/main/java/com/ssafy/realrealfinal/authms/common/exception/auth/UnexpectedTokenException.java
+++
@@ -1,8 +0,0 @@
-package com.ssafy.realrealfinal.authms.common.exception.auth;
-import com.ssafy.realrealfinal.authms.common.model.BaseException;
-import com.ssafy.realrealfinal.authms.common.exception.ErrorCode;
-public class UnexpectedTokenException extends BaseException {
-
-    public UnexpectedTokenException() {super(ErrorCode.UNEXPECTED_ERROR);}
-
-}
--- backend/userms/src/main/java/com/ssafy/realrealfinal/userms/db/repository/BoardRepository.java
+++
@@ -1,8 +0,0 @@
-package com.ssafy.realrealfinal.userms.db.repository;
-
-import com.ssafy.realrealfinal.userms.db.entity.Board;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BoardRepository extends JpaRepository<Board, Integer> {
-
-}
