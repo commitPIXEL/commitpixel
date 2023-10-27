56ddbd1ca46065894f932ac82d1a0282940460be
한다솜 <hmdasom@naver.com>
2023-10-27 10:09:17 +0900

    setting: spring cloud config를 위한 세팅

--- backend/imagems/src/main/java/com/ssafy/realrealfinal/imagems/ImagemsApplication.java
+++ backend/imagems/src/main/java/com/ssafy/realrealfinal/imagems/ImagemsApplication.java
@@ -2,10 +2,12 @@ package com.ssafy.realrealfinal.imagems;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @EnableFeignClients
+@EnableDiscoveryClient
 @EnableJpaRepositories(basePackages = "com.ssafy.realrealfinal")
 @SpringBootApplication
 public class ImagemsApplication {
