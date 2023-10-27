601b59cacf2aceba6f482edf9de766056b22fc19
이예린 <h2002y@naver.com>
2023-10-27 10:00:22 +0900

    Update frontend/src/app/browser/nav.tsx, frontend/src/app/config.ts

--- frontend/src/app/config.ts
+++ frontend/src/app/config.ts
@@ -2,5 +2,4 @@ export const width = 1024;
 export const height = 1024;
 
 export const socketUrl = process.env.NEXT_PUBLIC_SOCKET_URL;
-console.log("socketUrl: " + socketUrl);
 export const imageUrl = process.env.NEXT_PUBLIC_IMAGE_URL;
