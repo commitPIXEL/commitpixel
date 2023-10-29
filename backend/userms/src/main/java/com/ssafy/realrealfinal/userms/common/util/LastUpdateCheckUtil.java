38b5eec2c76787627cb168c877987f1441f3ead3
Amyhds <dasomhan524@gmail.com>
2023-10-29 13:30:16 +0900

    refactor: 최초 사용자, 15분 지난 상태, 15분 안 지난 상태를 나눠서 리턴

--- backend/userms/src/main/java/com/ssafy/realrealfinal/userms/common/util/LastUpdateCheckUtil.java
+++ backend/userms/src/main/java/com/ssafy/realrealfinal/userms/common/util/LastUpdateCheckUtil.java
@@ -2,6 +2,7 @@ package com.ssafy.realrealfinal.userms.common.util;
 
 import java.util.concurrent.ConcurrentNavigableMap;
 import java.util.concurrent.ConcurrentSkipListMap;
+
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
@@ -9,31 +10,47 @@ import org.springframework.stereotype.Component;
 @Component
 public class LastUpdateCheckUtil {
 
-    private final ConcurrentNavigableMap<String, Long> lastUpdateMap = new ConcurrentSkipListMap();
+    private final ConcurrentNavigableMap<Integer, Long> lastUpdateMap = new ConcurrentSkipListMap();
     private final long FIFTEEN_MINUTE = 15 * 60 * 1000;
 
     /**
-     * 마지막 업데이트 시간 저장
-     * @param userId
+     * 마지막 업데이트 시간 반환
+     *
+     * @param providerId
+     * @return
      */
-    public void updateTime(String userId) {
-        lastUpdateMap.put(userId, System.currentTimeMillis());
+    public Long getLastUpdateTime(Integer providerId) {
+        return lastUpdateMap.get(providerId);
+    }
+
+    /**
+     * 마지막 업데이트 시간 저장
+     *
+     * @param providerId
+     */
+    public void updateTime(Integer providerId) {
+        lastUpdateMap.put(providerId, System.currentTimeMillis());
     }
 
     /**
      * 마지막 업데이트로부터 15분 이후인지 확인
-     * @param userId
-     * @return
+     * 없는 사용자라면 map에 추가
+     *
+     * @param providerId
+     * @return Integer 0 = 새로운 사용자, 1 = 15분 지남, -1 = 15분 안 지남 
      */
-    public Boolean isPossibleToUpdate(String userId) {
-        // 없는 사용자라면 map에 추가 후 true 리턴
-        if (!lastUpdateMap.containsKey(userId)) {
-            updateTime(userId);
-            return true;
+    public Integer getLastUpdateStatus(Integer providerId) {
+        if (!lastUpdateMap.containsKey(providerId)) { // 없는 사용자
+            updateTime(providerId);
+            return 0;
         }
-        long lastUpdateTime = lastUpdateMap.get(userId);
+        long lastUpdateTime = lastUpdateMap.get(providerId);
         long currentTime = System.currentTimeMillis();
-        return (currentTime - lastUpdateTime) > FIFTEEN_MINUTE;
+        if ((currentTime - lastUpdateTime) >= FIFTEEN_MINUTE) { // 15분 이후
+            return 1;
+        } else { // 15분 미만
+            return -1;
+        }
     }
 
 }
