0a872986bed3238619a472d4480a96310d4d18ed
이예린 <h2002y@naver.com>
2023-10-30 15:26:56 +0900

    Update frontend/src/app/browser/sidebar/accordion/pickerAccorion.tsx

--- frontend/src/app/browser/sidebar/accordion/pickerAccorion.tsx
+++ frontend/src/app/browser/sidebar/accordion/pickerAccorion.tsx
@@ -10,8 +10,8 @@ const PickerAccordion = ({isConnected}: {
     <Accordion disabled={!isConnected} defaultExpanded={true && isConnected} className="!rounded mb-6">
       <AccordionTitle title="색깔" />
       <AccordionDetails className="flex flex-col justify-center items-center pt-4 rounded-b">
-        <Picker />
         <Menu />
+        <Picker />
       </AccordionDetails>
     </Accordion>
   );
