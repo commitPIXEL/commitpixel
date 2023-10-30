a454be6e756479b2186283795adb2c674ed878cc
한다솜 <hmdasom@naver.com>
2023-10-30 17:49:19 +0900

    feat: providerId를 map에 저장하도록 수정

--- backend/pixelms/src/main/java/com/ssafy/realrealfinal/pixelms/api/pixel/handler/WebSocketHandler.java
+++ backend/pixelms/src/main/java/com/ssafy/realrealfinal/pixelms/api/pixel/handler/WebSocketHandler.java
@@ -13,6 +13,7 @@ import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
+import java.util.Map;
 import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
 
@@ -23,19 +24,28 @@ public class WebSocketHandler {
 
     private final SocketIOServer server;
 
-    //연결된 클라이언트의 Websocket 세션을 저장
-    private static final ConcurrentHashMap<UUID, SocketIOClient> CLIENTS = new ConcurrentHashMap<>();
+    // 클라이언트의 providerId가 key, 연결된 클라이언트의 Websocket 세션이 value
+    private static final ConcurrentHashMap<UUID, Map<Integer, SocketIOClient>> CLIENTS = new ConcurrentHashMap<>();
 
     @EventListener(ContextRefreshedEvent.class)
     public void start() {
         server.start();
     }
 
-    // 클라이언트가 연결될 때 실행되는 메서드
+    // 클라이언트가 최초로 연결될 때 실행되는 메서드
     @OnConnect
     public void onConnect(SocketIOClient client) {
         log.info("New client connected: {}", client.getSessionId().toString());
-        CLIENTS.put(client.getSessionId(), client);
+
+        Map<Integer, SocketIOClient> socketMap = new ConcurrentHashMap<>();
+        String accessToken = client.getHandshakeData().getHttpHeaders().get("Authorization");
+        if (accessToken == null || accessToken.isEmpty() || accessToken.equals("")) {
+            socketMap.put(-1, client);
+        } else {
+            Integer providerId = 1; // TODO: feign으로 auth와 연결
+            socketMap.put(providerId, client);
+        }
+        CLIENTS.put(client.getSessionId(), socketMap);
     }
 
     // "pixel" 이벤트가 발생했을 때 실행되는 메서드
@@ -45,9 +55,11 @@ public class WebSocketHandler {
         if (CLIENTS == null || CLIENTS.size() == 0) {
             return;
         }
-        for (SocketIOClient clientSession : CLIENTS.values()) {
-            if (!client.getSessionId().equals(clientSession.getSessionId())&&clientSession.isChannelOpen()) {
-                clientSession.sendEvent("pixel", pixelDtoList);
+        for (Map<Integer, SocketIOClient> clientMap : CLIENTS.values()) {
+            for (SocketIOClient clientSession : clientMap.values()) {
+                if (!client.getSessionId().equals(clientSession.getSessionId()) && clientSession.isChannelOpen()) {
+                    clientSession.sendEvent("pixel", pixelDtoList);
+                }
             }
         }
     }
