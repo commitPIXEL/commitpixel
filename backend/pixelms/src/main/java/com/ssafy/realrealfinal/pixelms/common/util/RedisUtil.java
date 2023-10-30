6ae4de0b464e061fb43634e2ac0e516d47e06386
한다솜 <hmdasom@naver.com>
2023-10-30 17:47:17 +0900

    feat: redis util 추가

---
+++ backend/pixelms/src/main/java/com/ssafy/realrealfinal/pixelms/common/util/RedisUtil.java
@@ -0,0 +1,38 @@
+package com.ssafy.realrealfinal.pixelms.common.util;
+
+import com.ssafy.realrealfinal.pixelms.common.model.pixel.RedisNotFoundException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.redis.core.HashOperations;
+import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+
+@Slf4j
+@RequiredArgsConstructor
+@Component
+public class RedisUtil {
+
+    private final StringRedisTemplate stringRedisTemplate;
+
+    public Integer getData(String key, String type) throws RedisNotFoundException {
+        log.info("getData start: " + key + " " + type);
+        HashOperations<String, String, String> hashOperations = stringRedisTemplate.opsForHash();
+        log.warn("getData mid: " + hashOperations.get(key, type));
+        Integer data = Integer.parseInt(hashOperations.get(key, type));
+        log.info("getData end: " + data);
+        return data;
+
+    }
+
+
+    public void setData(String key, String type, Integer value) {
+        log.info("setData start: " + key + " " + type + " " + value);
+        HashOperations<String, String, String> hashOperations = stringRedisTemplate.opsForHash();
+        hashOperations.put(key, type, value.toString());
+        log.info("setData end: success");
+    }
+
+
+}
