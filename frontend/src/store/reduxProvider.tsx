ab684053043d852ea1dc489a3c94df3cda911bbf
Inbeom Kim <dlsqja970210@naver.com>
2023-10-30 16:10:11 +0900

    feat: 리덕스 영속성 라이브러리 추가

--- frontend/src/store/reduxProvider.tsx
+++ frontend/src/store/reduxProvider.tsx
@@ -1,8 +1,14 @@
 "use client";
 
-import Store from "../store/index";
+import Store, {persistor} from "../store/index";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
 
 export function ReduxProvider({ children }: { children: React.ReactNode }) {
-  return <Provider store={Store}>{children}</Provider>;
+  return (
+    <Provider store={Store}>
+      <PersistGate loading={null} persistor={persistor}>
+        {children}
+      </PersistGate>
+    </Provider>);
 }
