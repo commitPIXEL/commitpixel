9bdfdf9f3e9e06da24fbdbea457ac08902d13bb5
heejo <heejo515@naver.com>
2023-10-30 11:12:53 +0900

    feat: feign testcode 추가

--- backend/authms/src/main/java/com/ssafy/realrealfinal/authms/api/auth/controller/AuthController.java
+++ backend/authms/src/main/java/com/ssafy/realrealfinal/authms/api/auth/controller/AuthController.java
@@ -11,6 +11,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -85,7 +86,7 @@ public class AuthController {
     }
 
 
-    @GetMapping("/feigntest")
+    @PostMapping("/feigntest")
     String feignTest(@RequestBody String test) {
         log.info("feignTest start: " + test);
         String result = test + " connected by feign";
