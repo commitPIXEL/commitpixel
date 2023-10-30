aac0e3f4a61c0e4f9fc8db2027769fe63075f1e8
한다솜 <hmdasom@naver.com>
2023-10-30 17:48:51 +0900

    feat: redis 저장 로직 옮기는 중

--- backend/pixelms/src/main/java/com/ssafy/realrealfinal/pixelms/api/pixel/service/PixelServiceImpl.java
+++ backend/pixelms/src/main/java/com/ssafy/realrealfinal/pixelms/api/pixel/service/PixelServiceImpl.java
@@ -1,5 +1,92 @@
 package com.ssafy.realrealfinal.pixelms.api.pixel.service;
 
+import com.ssafy.realrealfinal.pixelms.common.model.pixel.RedisNotFoundException;
+import com.ssafy.realrealfinal.pixelms.common.util.RedisUtil;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@RequiredArgsConstructor
+@Service
 public class PixelServiceImpl implements PixelService {
+
+    private final RedisUtil redisUtil;
+    private final String TOTAL_CREDIT_KEY = "total";
+    private final String USED_PIXEL_KEY = "used";
+
+    /**
+     * 누적 사용 픽셀 수 업데이트
+     *
+     * @param providerId
+     */
+    @Override
+    public void updateUsedPixel(Integer providerId) {
+        log.info("updateUsedPixel start: " + providerId);
+
+        Integer usedPixel = redisUtil.getData(String.valueOf(providerId), USED_PIXEL_KEY);
+        redisUtil.setData(String.valueOf(providerId), USED_PIXEL_KEY, usedPixel + 1);
+
+        log.info("updateUsedPixel end");
+    }
+
+    /**
+     * 가용 픽셀 수 반환
+     *
+     * @param providerId
+     * @return
+     */
+    @Override
+    public Integer getAvailableCredit(Integer providerId) {
+        log.info("getTotalAndAvailableCredit start: " + providerId);
+
+        Integer totalCredit = getCredit(providerId, TOTAL_CREDIT_KEY);
+        Integer usedPixel = getCredit(providerId, USED_PIXEL_KEY);
+        Integer availableCredit = totalCredit - usedPixel;
+
+        log.info("getTotalAndAvailableCredit end: " + availableCredit);
+        return availableCredit;
+    }
+
+    /**
+     * 크레딧 반환 메서드
+     * 없다면(최초 가입) 0으로 set
+     *
+     * @param providerId providerId
+     * @param type       전체크레딧 또는 누적 사용픽셀수를 의미. total, used
+     * @return Integer 크레딧
+     */
+    private Integer getCredit(Integer providerId, String type) {
+        log.info("getCredit start: " + providerId + " " + type);
+        String key = String.valueOf(providerId);
+        Integer credit = 0;
+        try {
+            credit = redisUtil.getData(key, type);
+            log.info("getCredit end: " + credit);
+            return credit;
+        } catch (RedisNotFoundException e) {
+            redisUtil.setData(key, type, 0);
+            log.warn("getCredit end: " + 0);
+            return 0;
+        }
+
+    }
+
+    /**
+     * 전체 크레딧 업데이트
+     *
+     * @param providerId 깃허브 providerId
+     * @param additionalCredit 추가 크레딧 수
+     */
+    private void updateTotalCredit(Integer providerId, Integer additionalCredit) {
+        log.info("updateTotalCredit start: " + providerId + ", " + additionalCredit);
+
+        Integer totalCredit = getCredit(providerId, TOTAL_CREDIT_KEY);
+        redisUtil.setData(String.valueOf(providerId), TOTAL_CREDIT_KEY,
+                totalCredit + additionalCredit);
+
+        log.info("updateTotalCredit end");
+    }
+
 
 }
