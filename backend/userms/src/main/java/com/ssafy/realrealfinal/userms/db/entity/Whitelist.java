d6e0dadf25fcf419b8e8154c6ae7241018b7c3d5
Inbeom Kim <dlsqja970210@naver.com>
2023-10-31 14:28:27 +0900

    feat: whitelist Entity 생성

---
+++ backend/userms/src/main/java/com/ssafy/realrealfinal/userms/db/entity/Whitelist.java
@@ -0,0 +1,33 @@
+package com.ssafy.realrealfinal.userms.db.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Getter
+@ToString
+@NoArgsConstructor
+@Table(name = "whitelist")
+@Entity
+public class Whitelist {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "whitelist_id")
+    private Integer id;
+
+    @Column(name = "domain")
+    private String url;
+
+    @Builder
+    public Whitelist(String url) {
+        this.url = url;
+    }
+}
---
+++ backend/userms/src/main/java/com/ssafy/realrealfinal/userms/db/repository/WhitelistRepository.java
@@ -0,0 +1,9 @@
+package com.ssafy.realrealfinal.userms.db.repository;
+
+import com.ssafy.realrealfinal.userms.db.entity.Whitelist;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface WhitelistRepository extends JpaRepository<Whitelist, Integer> {
+
+    Whitelist findByUrl(String url);
+}
