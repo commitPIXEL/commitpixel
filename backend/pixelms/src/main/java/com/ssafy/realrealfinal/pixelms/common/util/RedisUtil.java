960b0e8ebd2e1aa810d8fe92179427de72b4e907
한다솜 <hmdasom@naver.com>
2023-10-31 16:58:01 +0900

    feat: getData, setData를 리턴과 파라미터 형식에 따라 메서드 분리

--- backend/pixelms/src/main/java/com/ssafy/realrealfinal/pixelms/common/util/RedisUtil.java
+++ backend/pixelms/src/main/java/com/ssafy/realrealfinal/pixelms/common/util/RedisUtil.java
@@ -7,8 +7,6 @@ import org.springframework.data.redis.core.HashOperations;
 import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.stereotype.Component;
 
-import java.util.Map;
-
 @Slf4j
 @RequiredArgsConstructor
 @Component
@@ -16,7 +14,7 @@ public class RedisUtil {
 
     private final StringRedisTemplate stringRedisTemplate;
 
-    public Integer getData(String key, String type) throws RedisNotFoundException {
+    public Integer getIntegerData(String key, String type) throws RedisNotFoundException {
         log.info("getData start: " + key + " " + type);
         HashOperations<String, String, String> hashOperations = stringRedisTemplate.opsForHash();
         log.warn("getData mid: " + hashOperations.get(key, type));
@@ -26,8 +24,25 @@ public class RedisUtil {
 
     }
 
+    public String getStringData(String key, String type) throws RedisNotFoundException {
+        log.info("getData start: " + key + " " + type);
+        HashOperations<String, String, String> hashOperations = stringRedisTemplate.opsForHash();
+        log.warn("getData mid: " + hashOperations.get(key, type));
+        String data = hashOperations.get(key, type);
+        log.info("getData end: " + data);
+        return data;
+
+    }
+
 
     public void setData(String key, String type, Integer value) {
+        log.info("setData start: " + key + " " + type + " " + value);
+        HashOperations<String, String, String> hashOperations = stringRedisTemplate.opsForHash();
+        hashOperations.put(key, type, value.toString());
+        log.info("setData end: success");
+    }
+
+    public void setData(String key, String type, String value) {
         log.info("setData start: " + key + " " + type + " " + value);
         HashOperations<String, String, String> hashOperations = stringRedisTemplate.opsForHash();
         hashOperations.put(key, type, value.toString());
