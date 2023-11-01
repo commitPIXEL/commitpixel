0cc755a319ba6bfec271775d8b090ec58c33c2a3
heejo <heejo515@naver.com>
2023-11-01 10:53:37 +0900

    feat: pixel Controller 설정중

--- backend/pixelms/src/main/java/com/ssafy/realrealfinal/pixelms/api/pixel/controller/PixelController.java
+++ backend/pixelms/src/main/java/com/ssafy/realrealfinal/pixelms/api/pixel/controller/PixelController.java
@@ -1,5 +1,15 @@
 package com.ssafy.realrealfinal.pixelms.api.pixel.controller;
 
-public class PixelController {
+import com.ssafy.realrealfinal.pixelms.api.pixel.service.PixelService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
+@RequiredArgsConstructor
+@RequestMapping("/pixel")
+@RestController
+public class PixelController {
+    private final PixelService pixelService;
 }
