82611439cae64d6d095c5e111eda6469983b0908
Inbeom Kim <dlsqja970210@naver.com>
2023-11-01 22:22:34 +0900

    feat: 리덕스에 user 추가

--- frontend/src/store/slices/userSlice.ts
+++ frontend/src/store/slices/userSlice.ts
@@ -5,23 +5,49 @@ const initialState = {
   profileImage: "",
   totalPixel: 0,
   availablePixel: 0,
-  url: ""
+  url: "",
 };
 
 const userSlice = createSlice({
   name: "user",
   initialState,
-  reducers:{
-    // payload type: string
-    login: (state, action: PayloadAction<string>) => {
-      state.nickName = action.payload;
+  reducers: {
+    getUserInfo: (
+      state,
+      action: PayloadAction<{
+        nickName: string;
+        profileImage: string;
+        url: string;
+      }>
+    ) => {
+      state.nickName = action.payload.nickName;
+      state.profileImage = action.payload.profileImage;
+      state.url = action.payload.url;
+    },
+    getUserPixel: (
+      state,
+      action: PayloadAction<{ totalPixel: number; availablePixel: number }>
+    ) => {
+      state.totalPixel = action.payload.totalPixel;
+      state.availablePixel = action.payload.availablePixel;
+    },
+    pickPixel: (state) => {
+      state.availablePixel -= 1;
+    },
+    addPixel: (state, action: PayloadAction<number>) => {
+      state.totalPixel += action.payload;
+      state.availablePixel += action.payload;
     },
     logout: (state) => {
       state.nickName = "";
-    }
-  }
+      state.profileImage = "";
+      state.totalPixel = 0;
+      state.availablePixel = 0;
+      state.url = "";
+    },
+  },
 });
 
-export const { login, logout } = userSlice.actions;
+export const { getUserInfo, getUserPixel, pickPixel, addPixel, logout } =
+  userSlice.actions;
 export default userSlice.reducer;
-
