dd8590f605745d0c463ec57885722161534a88ab
한다솜 <hmdasom@naver.com>
2023-11-01 17:46:05 +0900

    feat: providerId-nickname 맵 구현 중

---
+++ backend/pixelms/src/main/java/com/ssafy/realrealfinal/pixelms/common/util/NicknameProviderUtil.java
@@ -0,0 +1,26 @@
+package com.ssafy.realrealfinal.pixelms.common.util;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+
+@Slf4j
+@Component
+public class NicknameProviderUtil {
+
+    private HashMap<String, Integer> githubInfoMap = new HashMap<>();
+
+    public String getNickname() {
+        return null;
+    }
+
+    public Integer getProviderId() {
+        return null;
+    }
+
+    public void updateNickname() {
+
+    }
+
+}
