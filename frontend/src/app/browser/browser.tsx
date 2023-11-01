875c7c32c438496c09d919bf503638c35e1b4c12
김인범 <dlsqja970210@naver.com>
2023-11-02 08:59:17 +0900

    Add: redux에 user 상태 관리

--- frontend/src/app/browser/browser.tsx
+++ frontend/src/app/browser/browser.tsx
@@ -1,20 +1,54 @@
 "use client";
-import { useDispatch } from "react-redux";
+
+import { useState, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { getUserInfo, getUserPixel } from "@/store/slices/userSlice";
+import { RootState } from "@/store";
 import CanvasContainer from "./canvas/canvasContainer";
 import Nav from "./nav";
 import Sidebar from "./sidebar/sidebar";
 import { setTool } from "@/store/slices/toolSlice";
 import TestCanvas from "./canvas/testCanvas";
+import useFetchWithAuth from "@/hooks/useFetchWithAuth";
+import { UserInfo, UserFixel } from "../interfaces/browser";
 
 const Browser = () => {
   const dispatch = useDispatch();
+  const customFetch = useFetchWithAuth();
+  const accessToken = useSelector(
+    (state: RootState) => state.authorization.authorization
+  );
+  const user = useSelector((state: RootState) => state.user);
+
+  useEffect(() => {
+    const fetchAsync = async () => {
+      try {
+        const [resFromUser, resFromFixel] = await Promise.all([
+          customFetch("/user"),
+          customFetch("/fixel"),
+        ]);
+
+        const userData: UserInfo = await resFromUser.json();
+        const fixelData: UserFixel = await resFromFixel.json();
+        dispatch(getUserInfo(userData));
+        dispatch(getUserPixel(fixelData));
+      } catch (err) {
+        console.error("Error:", err);
+      }
+    };
+
+    if (accessToken.length) {
+      fetchAsync();
+    }
+  }, [accessToken]);
+
   const handleKeyDown = (e: React.KeyboardEvent) => {
     console.log(e.key);
-    if (e.key === 'h' || e.key === 'H') {
+    if (e.key === "h" || e.key === "H") {
       dispatch(setTool("panning"));
-    } else if (e.key === 'b' || e.key === 'B') {
+    } else if (e.key === "b" || e.key === "B") {
       dispatch(setTool("painting"));
-    } else if (e.key === 'i' || e.key === 'I') {
+    } else if (e.key === "i" || e.key === "I") {
       dispatch(setTool("copying"));
     } else if (e.key === "Escape") {
       dispatch(setTool(null));
@@ -22,15 +56,15 @@ const Browser = () => {
   };
   return (
     <main onKeyDown={handleKeyDown} className="w-screen h-screen bg-bgColor">
-          <Nav />
-          <div className="grid grid-cols-4 w-full h-[92%] place-items-center">
-            {/* TODO: 개발 버전과 Test버전 캔버스 잘 구별하기 */}
-            <CanvasContainer />
-            {/* <TestCanvas /> */}
-            <Sidebar />
-          </div>
+      <Nav />
+      <div className="grid grid-cols-4 w-full h-[92%] place-items-center">
+        {/* TODO: 개발 버전과 Test버전 캔버스 잘 구별하기 */}
+        <CanvasContainer />
+        {/* <TestCanvas /> */}
+        <Sidebar />
+      </div>
     </main>
   );
-}
+};
 
 export default Browser;
--- frontend/src/store/reducers/rootReducer.ts
+++ frontend/src/store/reducers/rootReducer.ts
@@ -1,14 +1,16 @@
 import { combineReducers } from "redux";
 import colorReducer from "../slices/colorSlice";
-import authorizationReducer from "../slices/authorizationSlice"
+import authorizationReducer from "../slices/authorizationSlice";
 import toolReducer from "../slices/toolSlice";
 import deviceReducer from "../slices/deviceSlice";
+import userReducer from "../slices/userSlice";
 
 const rootReducer = combineReducers({
-    color: colorReducer,
-    authorization: authorizationReducer,
-    tool: toolReducer,
-    device: deviceReducer,
+  color: colorReducer,
+  authorization: authorizationReducer,
+  tool: toolReducer,
+  device: deviceReducer,
+  user: userReducer,
 });
 
 export default rootReducer;
