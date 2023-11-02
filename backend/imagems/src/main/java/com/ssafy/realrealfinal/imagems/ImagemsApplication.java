586539c3b7e97bd1d812e5c6de9f80ecd7bbb3d8
heejo <heejo515@naver.com>
2023-11-02 14:02:22 +0900

    test: pixel to s3 test

--- backend/imagems/src/main/java/com/ssafy/realrealfinal/imagems/ImagemsApplication.java
+++ backend/imagems/src/main/java/com/ssafy/realrealfinal/imagems/ImagemsApplication.java
@@ -6,7 +6,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-//@EnableScheduling
+@EnableScheduling
 @EnableFeignClients
 @EnableDiscoveryClient
 @SpringBootApplication
