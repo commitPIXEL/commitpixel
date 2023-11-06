a4d22102ed985b5278bbb359a963ff72e8442c84
yup8229@gmail.com <yup8229@gmail.com>
2023-11-06 10:00:13 +0900

    feature: cors필터 추가

--- backend/pixelms/src/main/java/com/ssafy/realrealfinal/pixelms/config/WebConfig.java
+++ backend/pixelms/src/main/java/com/ssafy/realrealfinal/pixelms/config/WebConfig.java
@@ -12,10 +12,11 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
-    public void addCorsMappings(CorsRegistry registry){
+    public void addCorsMappings(CorsRegistry registry) {
         registry
             .addMapping("/pixel/**")  // 허용할 URL 패턴 설정
-            .allowedOrigins("http://localhost:3000", "https://dev.commitpixel.com")  // 허용할 오리진(도메인) 설정
+            .allowedOrigins("http://localhost:3000",
+                "https://dev.commitpixel.com")  // 허용할 오리진(도메인) 설정
             .allowedMethods("GET", "POST", "PUT", "PATCH", "OPTIONS", "DELETE")  // 허용할 HTTP 메소드 설정
             .allowedHeaders("*")  // 허용할 헤더 설정
             .allowCredentials(true);
