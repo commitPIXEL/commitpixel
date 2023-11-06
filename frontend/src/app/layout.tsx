3ec5a677d5c7ee40eddf0851fcf79c9df3137b48
h2002y <h2002y@naver.com>
2023-11-06 14:05:56 +0900

    feat: SEO 태그 추가

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -9,7 +9,18 @@ const inter = Inter({ subsets: ['latin'] })
 export const metadata: Metadata = {
   title: 'commit Pixel',
   description: 'commit Pixel by SSAFY 9기 자율 프로젝트 진짜_진짜_최종_팀',
-}
+  applicationName: 'SSAFY 9기 진짜_진짜_최종_팀의 commit Pixel',
+  authors: [{
+    name: "이용현",
+  },{name: "김희조"}, {name: "권도현"}, {name: "한다솜"}, {name: "김인범"}, {name: "이예린"}],
+  generator: "Next.js",
+  keywords: ["commit Pixel", "commit pixel", "Commit Pixel", "commit", "pixel", "커밋", "깃허브", 
+  "픽셀", "커밋 픽셀", "깃허브 커밋", "백준", "solved.ac", "알고리즘 문제", "커밋하기", "픽셀 아트", 
+  "github", "Github", "Baekjoon", "baekjoon", "Solved.ac", "솔브드", "포트폴리오 홍보", "제품 홍보", "아트", "pixel art",
+  "dot", "도트", "도트 찍기", "도트 아트", "commit", "git", "canvas", "r/place", "공유 캔버스", "공유 픽셀 아트",
+  "url", "url홍보", "커밋하고 url홍보", "ssafy", "SSAFY", "ssafy 9기", "삼성청년SW아카데미"],
+  referrer: "origin",  
+};
 
 export default function RootLayout({
   children,
