6f07cdd538207a926ab0c849124e6f0f38f30780
이예린 <h2002y@naver.com>
2023-11-06 11:13:52 +0900

    Fix: 홍보 url 작성 시 버그 해결

--- frontend/src/hooks/useSocket.ts
+++ frontend/src/hooks/useSocket.ts
@@ -17,24 +17,21 @@ const useSocket = () => {
       });
       
       socket.on("connect", () => {
-        console.log("Socket connected", socket);
         setSocket(socket);
       });
       
       socket.on("disconnect", (error) => {
-        console.error("Socket disconnected: ", error);
         setSocket(undefined);
         alert("Socket disconnected: " + error);
       });
       
       socket.on("connect_error", (error) => {
-        console.log("Error connecting to socket:", error);
         setSocket(undefined);
         alert("Error connecting to socket");
       });
       return socket;
     } else {
-      console.log("socketUrl is empty");
+      alert("socketUrl is empty");
     }
   }
     
