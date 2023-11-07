a437354971bc4b679c383816db3aae9a5cacaab1
이예린 <h2002y@naver.com>
2023-11-07 13:34:55 +0900

    Add: Fe/feature/mobile tutorial 모바일 로그인 연동

--- frontend/src/app/mobile/mobileNav.tsx
+++ frontend/src/app/mobile/mobileNav.tsx
@@ -1,25 +1,42 @@
-import { faDoorOpen, faRightFromBracket } from "@fortawesome/free-solid-svg-icons";
+import { RootState } from "@/store";
+import { faRightFromBracket } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import Image from "next/image";
+import { useSelector } from "react-redux";
+import LoginBtn from "@/components/loginBtn";
 
 const MobileNav = () => {
+  const user = useSelector((state: RootState) => state.user);
   return (
     <div className="bg-mainColor text-textBlack pl-2 pr-2 w-full h-[50px] flex items-center justify-between text-xs">
       <div className="relative w-[15%] h-full">
-        <Image src="/icon.png" fill style={{
-          objectFit: "contain",
-        }} sizes="40px" priority alt="commit pixel logo" />
+        <Image
+          src="/icon.png"
+          fill
+          style={{
+            objectFit: "contain",
+          }}
+          sizes="40px"
+          priority
+          alt="commit pixel logo"
+        />
       </div>
-      <div>githubNick</div>
-      <div className="flex items-center">
-        <div className="line-clamp-1">{Number(2400).toLocaleString()}</div>
-        <div className="ml-1 mr-1">/</div>
-        <div>{Number(14293).toLocaleString()}</div>
-        <div className="ml-1">p</div>
-      </div>
-      <div className="text-sm text-bgColor flex justify-center items-center">
-        <FontAwesomeIcon icon={faRightFromBracket} />
-      </div>
+      { user?.githubNickname !== "" ?
+        <>
+          <div>{user.githubNickname}</div>
+          <div className="flex items-center">
+            <div className="line-clamp-1">
+              {user.availablePixel.toLocaleString()}
+            </div>
+            <div className="ml-1 mr-1">/</div>
+            <div>{user.totalCredit.toLocaleString()}</div>
+            <div className="ml-1">p</div>
+          </div>
+          <div className="text-sm text-bgColor flex justify-center items-center">
+            <FontAwesomeIcon icon={faRightFromBracket} />
+          </div>
+        </> : <LoginBtn color={"main"} />
+      }
     </div>
   );
 };
