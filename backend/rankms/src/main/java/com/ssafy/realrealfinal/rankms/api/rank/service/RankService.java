daa92c26c72160596d1eb23adeea0250cde9f570
yup8229@gmail.com <yup8229@gmail.com>
2023-11-07 16:06:27 +0900

    remove: merge를 위한 폴더 삭제

--- backend/rankms/src/main/java/com/ssafy/realrealfinal/rankms/api/rank/service/RankService.java
+++
@@ -1,5 +0,0 @@
-package com.ssafy.realrealfinal.rankms.api.rank.service;
-
-public interface RankService {
-
-}
--- backend/userms/src/main/java/com/ssafy/realrealfinal/userms/common/exception/user/APIRequestException.java
+++
@@ -1,9 +0,0 @@
-package com.ssafy.realrealfinal.userms.common.exception.user;
-
-import com.ssafy.realrealfinal.userms.common.exception.ErrorCode;
-import com.ssafy.realrealfinal.userms.common.model.BaseException;
-
-public class APIRequestException extends BaseException {
-    public APIRequestException() {super(ErrorCode.API_REQUEST_FAILED);}
-}
-
