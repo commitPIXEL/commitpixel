5e1c658e6d04cda9398e408d0fb0a4b8bc5f9850
이예린 <h2002y@naver.com>
2023-11-10 09:19:07 +0900

    Add: Fe/feature/ranking 랭킹 API 연동

--- frontend/src/app/browser/sidebar/accordion/rankAccordion.tsx
+++ frontend/src/app/browser/sidebar/accordion/rankAccordion.tsx
@@ -1,128 +1,24 @@
-import { Accordion, AccordionDetails } from "@mui/material";
+import { Accordion, AccordionDetails, CircularProgress } from "@mui/material";
 import AccordionTitle from "./accordionTitle";
 import RankItem from "../rank/rankItem";
 
-const RankAccordion = ({title, type}: {
+const RankAccordion = ({title, type, data, isLoading}: {
   title: string,
   type: string,
+  data: any[],
+  isLoading: boolean,
 }) => {
   return (
     <Accordion defaultExpanded={true} className="!rounded mb-6">
       <AccordionTitle title={title} />
       <AccordionDetails className="min-h-0 flex flex-col justify-center items-center pt-4 rounded-b">
-        <div className="w-full max-h-[200px] flex flex-col no-scrollbar overflow-y-auto">
-          {type === "url" ? urlData.map((item) => <RankItem key={item.rank} rankInfo={item} isUrl={true} />) : 
-          pixelData.map((item) => <RankItem key={item.rank} rankInfo={item} />)}
-        </div>
+        {isLoading ? <CircularProgress /> : <div className="w-full max-h-[200px] flex flex-col no-scrollbar overflow-y-auto">
+          {type === "url" ? data?.map((item, index) => <RankItem key={index} rankInfo={item} rank={index + 1} isUrl={true} />) : 
+          data?.map((item, index) => <RankItem key={index} rankInfo={item} rank={index + 1} />)}
+        </div>}
       </AccordionDetails>
     </Accordion>
   );
 };
-
-const pixelData = [ 
-  {
-    rank:1,
-    content: "githubNick",
-    pixels: 1000,
-  },
-  {
-    rank:2,
-    content: "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa",
-    pixels: 900,
-  },
-  {
-    rank:3,
-    content: "orange",
-    pixels: 800,
-  },
-  {
-    rank:4,
-    content: "eevee",
-    pixels: 700,
-  },
-  {
-    rank:5,
-    content: "browniie",
-    pixels: 600,
-  },
-  {
-    rank:6,
-    content: "teddybear",
-    pixels: 500,
-  },
-  {
-    rank:7,
-    content: "knotted",
-    pixels: 400,
-  },
-  {
-    rank:8,
-    content: "kurby",
-    pixels: 300,
-  },
-  {
-    rank:9,
-    content: "meow",
-    pixels: 200,
-  },
-  {
-    rank:10,
-    content: "mouse",
-    pixels: 100,
-  },
-];
-
-const urlData = [
-  {
-    rank:1,
-    content: "https://edu.ssafy.com/edu/main/index.do",
-    pixels: 1000,
-  },
-  {
-    rank:2,
-    content: "https://edu.ssafy.com/edu/main/index.do",
-    pixels: 900,
-  },
-  {
-    rank:3,
-    content: "https://edu.ssafy.com/edu/main/index.do",
-    pixels: 800,
-  },
-  {
-    rank:4,
-    content: "https://edu.ssafy.com/edu/main/index.do",
-    pixels: 700,
-  },
-  {
-    rank:5,
-    content: "https://edu.ssafy.com/edu/main/index.do",
-    pixels: 600,
-  },
-  {
-    rank:6,
-    content: "https://edu.ssafy.com/edu/main/index.do",
-    pixels: 500,
-  },
-  {
-    rank:7,
-    content: "https://edu.ssafy.com/edu/main/index.do",
-    pixels: 400,
-  },
-  {
-    rank:8,
-    content: "https://edu.ssafy.com/edu/main/index.do",
-    pixels: 300,
-  },
-  {
-    rank:9,
-    content: "https://edu.ssafy.com/edu/main/index.do",
-    pixels: 200,
-  },
-  {
-    rank:10,
-    content: "https://edu.ssafy.com/edu/main/index.do",
-    pixels: 100,
-  },
-];
 
 export default RankAccordion;
