52d13ff99b7df3a63f0a50439e260689f269c2c3
이예린 <h2002y@naver.com>
2023-11-10 11:31:53 +0900

    Update rankItem.tsx

--- frontend/src/app/browser/sidebar/rank/rankItem.tsx
+++ frontend/src/app/browser/sidebar/rank/rankItem.tsx
@@ -1,29 +1,30 @@
-const RankItem = ({ rankInfo, isUrl, rank }: {
+const RankItem = ({ rankInfo, isUrl }: {
   rankInfo: {
-    url: string,
-    pixelNum: number,
+    rank: number,
+    content?: string,
+    pixels: number,
+    githubNickname?: string,
   },
-  isUrl?: boolean,
-  rank: number
+  isUrl?: boolean
 }) => {
   const urlStyle = "cursor-pointer text-xs";
   let rankStyle = "";
-  if(rank === 1) {
+  if(rankInfo.rank === 1) {
     rankStyle = "text-[#FFCA10]";
-  } else if(rank === 2) {
+  } else if(rankInfo.rank === 2) {
     rankStyle = "text-[#BAD1D6]";
-  } else if(rank === 3) {
+  } else if(rankInfo.rank === 3) {
     rankStyle = "text-[#BC800A]";
   }
 
   return (
     <div className="cursor-default text-textBlack w-full min-h-[40px] grid grid-cols-10 gap-4 place-content-center place-items-center">
-      <div className={`place-self-start col-span-1 text-lg ${rankStyle}`}>{rank}</div>
+      <div className={`place-self-start col-span-1 text-lg ${rankStyle}`}>{rankInfo.rank}</div>
       <div className={`w-full h-full flex items-center col-span-5 line-clamp-1 ${isUrl ? urlStyle : ""}`}>
-        { isUrl ? <a href={rankInfo.url} target="_blank">{rankInfo.url}</a> : rankInfo.url}
+        { isUrl ? <a href={rankInfo.content} target="_blank">{rankInfo.content}</a> : rankInfo.githubNickname}
       </div>
       <div className="place-self-end col-span-4 line-clamp-1 flex items-center">
-        <div>{rankInfo.pixelNum}</div>
+        <div>{rankInfo.pixels}</div>
         <div className="ml-1 text-sm">pixels</div>
       </div>
     </div>
