64128808eabe14d2de24520ae0e7f70e32c17cc7
이예린 <h2002y@naver.com>
2023-11-10 14:29:09 +0900

    Fix: Fe/feature/image 이미지 처리 위치 변경

--- frontend/src/app/browser/sidebar/imageToPixel.tsx
+++ frontend/src/app/browser/sidebar/imageToPixel.tsx
@@ -1,14 +1,17 @@
 import axios from "axios";
-import { useCallback, useRef } from "react";
-import { apiUrl } from "../../config";
+import { useCallback, useRef, useState } from "react";
+import { apiUrl } from "../config";
+import { CircularProgress } from "@mui/material";
 
 const ImageToPixel = () => {
   const inputRef = useRef<HTMLInputElement | null>(null);
+  const [isLoading, setIsLoading] = useState(false);
 
   const handleUploadImage = useCallback((e: React.ChangeEvent<HTMLInputElement>) => {
     if (!e.target.files) {
       return;
     }
+    setIsLoading(true);
     const image = e.target.files[0];
     let formData = new FormData();
     formData.append('file', e?.target.files[0]);
@@ -19,7 +22,6 @@ const ImageToPixel = () => {
         "Content-Type": "multipart/form-data"
       }
     }).then((res) => {
-      console.log(res);
       const blob = new Blob([res.data], {type: "image/jpeg"});
       const url = window.URL.createObjectURL(blob);
       const link = document.createElement("a");
@@ -28,6 +30,7 @@ const ImageToPixel = () => {
       link.setAttribute("download", `pixeled_image_${date.toISOString().slice(0, 10)}.jpeg`);
       document.body.appendChild(link);
       link.click();
+      setIsLoading(false);
       document.body.removeChild(link);
       window.URL.revokeObjectURL(url);
     }).catch((err) => {
@@ -39,9 +42,11 @@ const ImageToPixel = () => {
   return (
     <>
       <label htmlFor="image_to_pixel" className="cursor-pointer drop-shadow-md w-[45%] bg-mainColor rounded min-h-[40px] flex justify-center items-center">
-        <div>
-          이미지 픽셀화
-        </div>
+        {isLoading ? (
+            <CircularProgress className="p-2" />
+          ) : 
+            "이미지 픽셀화"
+          }
       </label>
       <input className="hidden" ref={inputRef} onChange={handleUploadImage} id="image_to_pixel" type="file" accept="image/jpg, image/png, image/jpeg"></input>
     </>
