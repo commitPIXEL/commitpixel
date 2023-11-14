37f67b222aafc951820b6bc1db36f1d38e652aa4
yup8229@gmail.com <yup8229@gmail.com>
2023-11-14 16:58:49 +0900

    fix: login 할 때 githubnickname 도 body에 담아서 넘겨주도록 수정

--- backend/authms/src/main/java/com/ssafy/realrealfinal/authms/api/auth/service/AuthServiceImpl.java
+++ backend/authms/src/main/java/com/ssafy/realrealfinal/authms/api/auth/service/AuthServiceImpl.java
@@ -55,7 +55,7 @@ public class AuthServiceImpl implements AuthService {
             String jwtAccessToken = jwtUtil.createAccessToken(providerId);
             saveTokens(oauthUserRes.getId().toString(), jwtRefreshToken,
                 jsonToken.getAccessToken());
-            TokenRes tokenRes = AuthMapper.INSTANCE.toTokenRes(jwtAccessToken, jwtRefreshToken);
+            TokenRes tokenRes = AuthMapper.INSTANCE.toTokenRes(jwtAccessToken, jwtRefreshToken,oauthUserRes.getLogin());
             log.info("login end: " + tokenRes);
             return tokenRes;
         } else {
