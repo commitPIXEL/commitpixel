223dc21e356f86c6746560216d5fe6f49b9d75d3
Inbeom Kim <dlsqja970210@naver.com>
2023-11-14 14:51:45 +0900

    feat: 모바일 화면에서 사용자 정보 API 연결

--- frontend/src/app/mobile/mobile.tsx
+++ frontend/src/app/mobile/mobile.tsx
@@ -1,13 +1,49 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import CanvasContainer from "../browser/canvas/canvasContainer";
 import Menu from "../browser/sidebar/picker/menu/pickerMenu";
 import MobileNav from "./mobileNav";
 import MobilePicker from "./mobilePicker";
 import { RootState } from "@/store";
 import LoginBtn from "@/components/loginBtn";
+import useFetchWithAuth from "@/hooks/useFetchWithAuth";
+import { useEffect } from "react";
+import { getUserInfo, updateUserPixel } from "@/store/slices/userSlice";
+import { IUserInfo, IUserPixel } from "@/interfaces/browser";
 
 const Mobile = () => {
+  const dispatch = useDispatch();
+  const customFetch = useFetchWithAuth();
   const user = useSelector((state: RootState) => state.user);
+  const accessToken = useSelector(
+    (state: RootState) => state.authorization.authorization
+  );
+
+  useEffect(() => {
+    if (accessToken && !user.githubNickname) {
+      fetchAsync();
+    }
+  }, [accessToken]);
+
+  const fetchAsync = async () => {
+    try {
+      const resUser = await customFetch("/user/");
+      const userData: IUserInfo = await resUser.json();
+
+      dispatch(getUserInfo(userData));
+    } catch (err) {
+      console.error("Error:", err);
+    }
+
+    try {
+      const resPixel = await customFetch("/user/refreshinfo");
+      const pixelData: IUserPixel = await resPixel.json();
+
+      dispatch(updateUserPixel(pixelData));
+    } catch (err) {
+      console.error("Error:", err);
+    }
+  };
+
 
   return(
   <div className="w-screen h-screen items-center flex flex-col bg-bgColor text-white">
