7e593222ff676da01abc13100f654e83c45d6d5a
Inbeom Kim <dlsqja970210@naver.com>
2023-11-15 14:50:49 +0900

    fix: 사용자 정보 병렬 요청 처리로 변경 및 예외 경우 console

--- frontend/src/hooks/useFetchUser.ts
+++ frontend/src/hooks/useFetchUser.ts
@@ -1,38 +1,31 @@
-import { RootState } from '@/store';
-import { apiUrl } from '@/app/browser/config';
 import { useDispatch } from "react-redux";
 import useFetchAuth from './useFetchAuth';
 import { getUserInfo, updateUserPixel } from '@/store/slices/userSlice';
-import { IUserInfo, IUserPixel } from '@/interfaces/browser';
 
 
 const useFetchUser = async () => {
     const dispatch = useDispatch();
     const customFetch = useFetchAuth();
-    console.log("useFetchUser 실행");
 
     try {
-        const resUser = await customFetch("/user/");
-        const userData: IUserInfo = await resUser.json();
-        dispatch(getUserInfo(userData));
+      const [resUser, resPixel] = await Promise.all([
+          customFetch("/user/"),
+          customFetch("/user/refreshinfo")
+      ]);
 
-        const resPixel = await customFetch("/user/refreshinfo");
-        const pixelData: IUserPixel = await resPixel.json();
-        dispatch(updateUserPixel(pixelData));
+      const [userData, pixelData] = await Promise.all([
+          resUser.json(),
+          resPixel.json()
+      ]);
 
-        return {success: true, userData, pixelData};
-      } catch (err) {
-        console.error("Error:", err);
-      }
-  
-      try {
-        const resPixel = await customFetch("/user/refreshinfo");
-        const pixelData: IUserPixel = await resPixel.json();
-  
-        dispatch(updateUserPixel(pixelData));
-      } catch (err) {
-        return { success: false, error: err };
-      }
+      dispatch(getUserInfo(userData));
+      dispatch(updateUserPixel(pixelData));
+
+      return { success: true, userData, pixelData };
+  } catch (err) {
+      console.error("Error:", err);
+      return { success: false, error: err };
+  }
 };
 
 export default useFetchUser;
