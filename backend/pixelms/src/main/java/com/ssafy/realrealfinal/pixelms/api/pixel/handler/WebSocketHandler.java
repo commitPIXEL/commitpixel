4d6e885984831a88a14d937f025805b85fcdc996
yup8229@gmail.com <yup8229@gmail.com>
2023-11-15 10:58:48 +0900

    feature: 토큰 버킷 알고리즘 추가 & getSesstionId() 호출 리팩토링

--- backend/pixelms/src/main/java/com/ssafy/realrealfinal/pixelms/api/pixel/handler/WebSocketHandler.java
+++ backend/pixelms/src/main/java/com/ssafy/realrealfinal/pixelms/api/pixel/handler/WebSocketHandler.java
@@ -10,6 +10,10 @@ import com.ssafy.realrealfinal.pixelms.api.pixel.feignClient.AuthFeignClient;
 import com.ssafy.realrealfinal.pixelms.api.pixel.response.PixelInfoRes;
 import com.ssafy.realrealfinal.pixelms.api.pixel.service.PixelService;
 import com.ssafy.realrealfinal.pixelms.common.util.RedisUtil;
+import io.github.bucket4j.Bandwidth;
+import io.github.bucket4j.Bucket;
+import io.github.bucket4j.Refill;
+import java.time.Duration;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.event.ContextRefreshedEvent;
@@ -29,18 +33,17 @@ public class WebSocketHandler {
     private final RedisUtil redisUtil;
     private final AuthFeignClient authFeignClient;
     private final SocketIOServer server;
-    private final int SCALE = 512;
-    private final long MIN_INTERVAL = 75; // 0.075초
-    private final String PIXEL = "pixel";
-    private final String IS_PIXEL_SUCCESS = "isPixelSuccess";
-    private final String TOO_FREQUENT = "tooFrequent";
-    private final String IS_NOT_USER = "isNotUser";
-    private final String AVAILABLE_CREDIT = "availableCredit";
-    private final String URL = "url";
+    private static final int SCALE = 512;
+    private static final String PIXEL = "pixel";
+    private static final String IS_PIXEL_SUCCESS = "isPixelSuccess";
+    private static final String TOO_FREQUENT = "tooFrequent";
+    private static final String IS_NOT_USER = "isNotUser";
+    private static final String AVAILABLE_CREDIT = "availableCredit";
+    private static final String URL = "url";
 
     // 연결된 클라이언트의 Websocket 세션이 key, {providerId, SocketIoClient}가 value
     private static final ConcurrentHashMap<UUID, SocketClientInfo> CLIENTS = new ConcurrentHashMap<>();
-    private static final ConcurrentHashMap<UUID, Long> lastRequestTimeMap = new ConcurrentHashMap<>();
+    private static final ConcurrentHashMap<UUID, Bucket> BUCKETS = new ConcurrentHashMap<>();
 
     /**
      * 스프링 애플리케이션 컨텍스트가 초기화되거나 새로 고쳐질 때 서버를 시작
@@ -62,7 +65,8 @@ public class WebSocketHandler {
      */
     @OnConnect
     public void onConnect(SocketIOClient client) {
-        log.info("New client connected: {}", client.getSessionId().toString());
+        UUID sessionId = client.getSessionId();
+        log.info("New client connected: {}", sessionId.toString());
 
         String userAgent = client.getHandshakeData().getHttpHeaders().get("User-Agent");
         if (userAgent == null || !userAgent.contains("Mozilla/5.0")) {
@@ -72,6 +76,11 @@ public class WebSocketHandler {
             return;
         }
 
+        // 접속한 클라이언트에 대한 토큰 버킷 생성 테스트용으로 10초에 10번만 요청할 수 있도록 변경
+        Bucket bucket = Bucket.builder()
+            .addLimit(Bandwidth.classic(10, Refill.greedy(10, Duration.ofSeconds(10)))).build();
+
+        BUCKETS.put(sessionId, bucket);
 
         String accessToken = client.getHandshakeData().getSingleUrlParam("Authorization");
         // 닉네임이 바뀌는 경우는 이미 새롭게 로그인을 하고 프론트에 새로운 닉네임이 있는 상태
@@ -85,7 +94,7 @@ public class WebSocketHandler {
             // header로 온 accessToken을 auth로 feign 요청을 보내서 providerId를 얻음
             providerId = authFeignClient.withQueryString(accessToken);
         }
-        CLIENTS.put(client.getSessionId(), new SocketClientInfo(providerId, client));
+        CLIENTS.put(sessionId, new SocketClientInfo(providerId, client));
         // id-name redis 업데이트
         redisUtil.setData(providerId + ":name", githubNickname);
     }
@@ -99,36 +108,34 @@ public class WebSocketHandler {
      */
     @OnEvent("pixel")
     public void onPixelEvent(SocketIOClient client, List pixelInfo) {
+        UUID sessionId = client.getSessionId();
         log.info("Pixel event received: {}", pixelInfo);
         // 사용자가 없을 때
         if (CLIENTS == null || CLIENTS.size() == 0) {
             return;
         }
 
-        Integer providerId = CLIENTS.get(client.getSessionId()).getProviderId();
+        Integer providerId = CLIENTS.get(sessionId).getProviderId();
 
         // 비회원이면 return
         if (providerId == -1) {
             return;
         }
 
-        UUID sessionId = client.getSessionId();
-        long currentTime = System.currentTimeMillis();
+        Integer availableCredit = pixelService.getAvailableCredit(providerId);
 
-        // 마지막 요청 시간 확인
-        Long lastRequestTime = lastRequestTimeMap.get(sessionId);
-
-        // 이전 요청이 있었고, 현재 요청이 최소 간격보다 짧은 경우
-        if (lastRequestTime != null && (currentTime - lastRequestTime) < MIN_INTERVAL) {
-            // 요청 거부
-            client.sendEvent(TOO_FREQUENT);
+        // 사용 가능 픽셀이 0개일 때
+        if (availableCredit == 0) {
+            // 픽셀 정보 업데이트 실패!
+            client.sendEvent(IS_PIXEL_SUCCESS, false);
             return;
         }
 
-        // 사용 가능 픽셀이 0개일 때
-        if(pixelService.getAvailableCredit(providerId) == 0) {
-            // 픽셀 정보 업데이트 실패!
-            client.sendEvent(IS_PIXEL_SUCCESS, false);
+        Bucket bucket = BUCKETS.get(sessionId);
+
+        if (bucket == null || !bucket.tryConsume(1)) {
+            // 토큰이 없으면 요청 거부
+            client.sendEvent(TOO_FREQUENT);
             return;
         }
 
@@ -137,20 +144,16 @@ public class WebSocketHandler {
 
         // 현재 클라이언트의 usedPixel redis 값 변경 후 클라이언트에게 반환
         pixelService.updateUsedPixel(providerId);
-        Integer availableCredit = pixelService.getAvailableCredit(providerId);
         client.sendEvent(AVAILABLE_CREDIT, availableCredit);
 
         // 나를 제외한 모든 사용자에게 픽셀 변경 사항을 보내줌
         for (SocketClientInfo clientInfo : CLIENTS.values()) {
             SocketIOClient clientSession = clientInfo.getSocketIOClient();
-            if (!client.getSessionId().equals(clientSession.getSessionId())
-                    && clientSession.isChannelOpen()) {
+            if (!sessionId.equals(clientSession.getSessionId())
+                && clientSession.isChannelOpen()) {
                 clientSession.sendEvent(PIXEL, pixelInfo);
             }
         }
-
-        // 마지막 요청 시간 업데이트
-        lastRequestTimeMap.put(sessionId, currentTime);
 
         // 픽셀 정보 성공적으로 업데이트됨
         client.sendEvent(IS_PIXEL_SUCCESS, true);
@@ -179,8 +182,9 @@ public class WebSocketHandler {
      */
     @OnDisconnect
     public void onDisconnectEvent(SocketIOClient client) {
-        log.info("Client disconnected: {}", client.getSessionId().toString());
-        lastRequestTimeMap.remove(client.getSessionId());
-        CLIENTS.remove(client.getSessionId());
+        UUID sessionId = client.getSessionId();
+        log.info("Client disconnected: {}", sessionId.toString());
+        BUCKETS.remove(sessionId);
+        CLIENTS.remove(sessionId);
     }
 }
